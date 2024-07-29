1cf462d42622134c1f44c820ee3a37bd2ee73deb
JUNSEO CHOI <myqkq111@naver.com>
2024-07-29 14:32:29 +0900

    작업 패키지 생성 + MemberVO 작성

---
+++ src/main/java/vo/ProductVO.java
@@ -0,0 +1,16 @@
+package vo;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+
+    private int id;
+    private String cate;
+    private String name;
+    private int price;
+    private String image;
+    private String content;
+    private String contentImage;
+
+}
