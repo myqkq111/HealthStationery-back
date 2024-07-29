f9f7a13da2d0ed7b38783e8e36f01bb9f91d3d62
JUNSEO CHOI <myqkq111@naver.com>
2024-07-29 18:39:27 +0900

    no message

--- src/main/java/com/example/finalproject/vo/RecentlyViewedItemsVO.java
+++ src/main/java/com/example/finalproject/vo/RecentlyViewedItemsVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class RecentlyViewedItemsVO {
 
-    private int id;
-
-
+    private int id; //최근 본 상품 고유번호
+    private int productId; //상품 고유번호
+    private int memberId; //회원 고유번호
 }
