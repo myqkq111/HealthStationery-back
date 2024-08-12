487d951c4489a3eedfd805d0b18e66b38b558601
JUNSEO CHOI <myqkq111@naver.com>
2024-08-12 11:23:38 +0900

    Feat : 장바구니 등록 기능 구현

--- src/main/java/com/example/finalproject/vo/BasketVO.java
+++ src/main/java/com/example/finalproject/vo/BasketVO.java
@@ -8,7 +8,7 @@ public class BasketVO {
     private int id; //장바구니 상품 번호
     private int productId; //상품 고유번호
     private int memberId; //회원 고유번호
-//    private String option;
-//    private String color;
-//    private int size;
+    private String color; //상품 컬러
+    private String size; //상품 사이즈
+    private String count; //상품 개수
 }
