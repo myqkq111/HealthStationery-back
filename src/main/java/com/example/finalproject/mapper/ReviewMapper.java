090d18f3a71688c6e6c2cbcc737bb8df6a77f2f9
realseongho <qotjdgh000@gmail.com>
2024-08-14 17:22:28 +0900

    Feat : 리뷰 작성 및 불러오기 기능 구현

---
+++ src/main/java/com/example/finalproject/mapper/ReviewMapper.java
@@ -0,0 +1,22 @@
+package com.example.finalproject.mapper;
+
+import com.example.finalproject.vo.ReviewVO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ReviewMapper {
+
+    // 리뷰 작성
+    @Insert("INSERT INTO review(buylist_id, member_id, score, content)" +
+            "VALUES(#{buylistId}, #{memberId}, #{score}, #{content})")
+    void insert(ReviewVO reviewVO);
+
+    // 리뷰 출력
+    @Select("SELECT content, score, name FROM review JOIN buylist b ON buylist_id = b.id " +
+            "WHERE product_id = #{productId}")
+    public List<ReviewVO> selectByProductId(int productId);
+}
--- src/main/java/com/example/finalproject/vo/ReviewVO.java
+++ src/main/java/com/example/finalproject/vo/ReviewVO.java
@@ -7,9 +7,8 @@ public class ReviewVO {
 
     private int id; //후기 고유번호
     private int buylistId; //구매 고유번호
-    private int memberId; //회원 고유번호
+    private int productId; //제품 고유번호
     private int score; //별점
     private String content; //후기 내용
-    private int like; //후기 좋아요
-    private String contentImage; //후기 이미지 제목
+    private String name;
 }
