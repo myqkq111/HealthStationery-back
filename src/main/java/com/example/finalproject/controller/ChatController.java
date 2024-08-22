f4e381d86caa8c364f268cd66bdc40f8088bf61f
leeminwook1 <dlalsldnr2303@naver.com>
2024-08-22 15:01:22 +0900

    Docs: 채팅 기능 구현

--- src/main/java/com/example/finalproject/controller/ChatController.java
+++ src/main/java/com/example/finalproject/controller/ChatController.java
@@ -1,10 +1,10 @@
 package com.example.finalproject.controller;
 
-
 import com.example.finalproject.entity.ChatMessage;
 import com.example.finalproject.service.ChatMessageService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,33 +24,30 @@ public class ChatController {
 
     @MessageMapping("/sendMessage")
     public void sendMessage(@Payload ChatMessage chatMessage) {
+        // 사용자 ID와 경로가 정확한지 확인
+        messagingTemplate.convertAndSend("/topic/admin/"+"11", chatMessage);
         chatMessageService.saveMessage(chatMessage.getMemberId(), chatMessage.getName(), chatMessage.getContent(), chatMessage.getSns());
-        messagingTemplate.convertAndSendToUser(
-                String.valueOf(chatMessage.getMemberId()), "/queue/messages", chatMessage);
+        System.out.println("Message sent to user ID: " + chatMessage.getTransmitMemberId());
     }
 
     @MessageMapping("/sendAdminMessage")
     public void sendAdminMessage(@Payload ChatMessage chatMessage) {
+        messagingTemplate.convertAndSend("/topic/admin/"+chatMessage.getTransmitMemberId(), chatMessage);
         chatMessageService.saveAdminMessage(chatMessage.getMemberId(), chatMessage.getName(), chatMessage.getContent(), chatMessage.getSns(), chatMessage.getTransmitMemberId());
-        messagingTemplate.convertAndSendToUser(
-                String.valueOf(chatMessage.getMemberId()), "/queue/messages", chatMessage);
     }
 
     @GetMapping("/history")
     public List<ChatMessage> getChatHistory(@RequestParam int memberId) {
-        System.out.println(chatMessageService.getChatHistory(memberId));
         return chatMessageService.getChatHistory(memberId);
     }
 
     @GetMapping("/historyAdmin")
     public List<ChatMessage> getChatHistoryAdmin(@RequestParam int memberId) {
-        System.out.println(chatMessageService.getAdminChatHistory(memberId));
         return chatMessageService.getAdminChatHistory(memberId);
     }
 
     @GetMapping("/rooms")
     public List<ChatMessage> getChatRooms() {
-        // 모든 채팅 메시지를 가져와서 반환
         return chatMessageService.getAllMessages();
     }
 }
